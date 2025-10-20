c57ad1b8b016419435d0586097f354efa624fab2
ebmarahofficial-bit <ebmarahofficial@gmail.com>
2025-10-20 14:03:50 -0700

    Update PreloadScene.js

--- gorillaman/js/scenes/PreloadScene.js
+++ gorillaman/js/scenes/PreloadScene.js
@@ -1,25 +1,24 @@
 class PreloadScene extends Phaser.Scene{
   constructor(){ super('PreloadScene'); }
   preload(){
-    // tiles & sprites
-    this.load.image('tiles', 'gorillaman/assets/tiles/maze.png');
-
-    // Use your 16x16 assets so they sit on the tile centers nicely
-    this.load.image('usb',    'gorillaman/assets/sprites/usb2.png');
-    this.load.image('shower', 'gorillaman/assets/sprites/showerhead2.png');
+    // Small, crisp assets so they sit in the 16x16 grid
+    this.load.image('usb',    'gorillaman/assets/sprites/usb_16.png');
+    this.load.image('shower', 'gorillaman/assets/sprites/shower_16.png');
     this.load.image('eyes',   'gorillaman/assets/sprites/eyes.png');
 
     this.load.spritesheet('gorilla','gorillaman/assets/sprites/gorilla.png',{ frameWidth:16, frameHeight:16 });
     this.load.spritesheet('wook','gorillaman/assets/sprites/wook.png',{ frameWidth:16, frameHeight:16 });
     this.load.spritesheet('wook_fright','gorillaman/assets/sprites/wook_fright.png',{ frameWidth:16, frameHeight:16 });
 
-    // Your JSON map
+    // Level
     this.load.json('level1','gorillaman/assets/maps/level1.json');
   }
   create(){
+    // Simple walk animations
     this.anims.create({ key:'g_walk', frames:this.anims.generateFrameNumbers('gorilla',{ start:0, end:3 }), frameRate:8, repeat:-1 });
     this.anims.create({ key:'w_walk', frames:this.anims.generateFrameNumbers('wook',{ start:0, end:3 }), frameRate:8, repeat:-1 });
     this.anims.create({ key:'w_fright', frames:this.anims.generateFrameNumbers('wook_fright',{ start:0, end:3 }), frameRate:8, repeat:-1 });
+
     this.scene.start('GameScene');
   }
 }
